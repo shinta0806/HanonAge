062b5974cb109281683abb959926e30e9615307c
SHINTA <shinta.0806.develop@hotmail.com>
2024-06-05 15:05:46 +0900

    デザイン作成。

--- HanonAge/Form1.Designer.cs
+++ HanonAge/Form1.Designer.cs
@@ -28,12 +28,61 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			label1 = new Label();
+			ButtonGo = new Button();
+			ButtonCancel = new Button();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(23, 19);
+			label1.Name = "label1";
+			label1.Size = new Size(157, 15);
+			label1.TabIndex = 0;
+			label1.Text = "選択された音符を半音上げます";
+			// 
+			// ButtonGo
+			// 
+			ButtonGo.Location = new Point(23, 49);
+			ButtonGo.Name = "ButtonGo";
+			ButtonGo.Size = new Size(75, 23);
+			ButtonGo.TabIndex = 1;
+			ButtonGo.Text = "実行";
+			ButtonGo.UseVisualStyleBackColor = true;
+			// 
+			// ButtonCancel
+			// 
+			ButtonCancel.Location = new Point(114, 49);
+			ButtonCancel.Name = "ButtonCancel";
+			ButtonCancel.Size = new Size(75, 23);
+			ButtonCancel.TabIndex = 2;
+			ButtonCancel.Text = "キャンセル";
+			ButtonCancel.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			AcceptButton = ButtonGo;
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			CancelButton = ButtonCancel;
+			ClientSize = new Size(210, 91);
+			Controls.Add(ButtonCancel);
+			Controls.Add(ButtonGo);
+			Controls.Add(label1);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "Form1";
+			Text = "半音上げ";
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private Label label1;
+		private Button ButtonGo;
+		private Button ButtonCancel;
 	}
 }
